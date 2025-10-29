2d5808b3321402505be384023439fda8b33611a5
766998758-glitch <766998758@qq.com>
2025-10-29 18:31:54 +0800

    更改了TIM定时器的中断间隔。终于在Vofa上输出了还算让人满意的波

--- User/main.c
+++ User/main.c
@@ -3,8 +3,8 @@
 #include "OLED.h"
 #include "Key.h"
 #include "Timer.h"
-#include "Cursor2.h"
-#include "Encoder.h"
+//#include "Cursor2.h"
+//#include "Encoder.h"
 #include "PWM.h"
 #include "TT130.h"
 #include "Serial.h"  //Õâ¸ö¶«Î÷ÏÈ±ğÓÃµÄ¡£µÈµç³ØÓĞµçÁËÔÙËµ¡£Ö±½ÓÈÃpacket[2]µÈÓÚTarget¾ÍĞĞ¡£
@@ -15,9 +15,9 @@
 //uint8_t menu_state = 0;
 float CNT,Out,Target;
 float error0,error1,ErrorInt;
-float kp = 0.6,ki = 0.2,kd = 0.7;
-uint32_t last_time = 0;
-uint32_t current_time = 0;
+float kp,ki,kd;
+//uint32_t last_time = 0;
+//uint32_t current_time = 0;
 
 
 int main(void)
@@ -25,51 +25,17 @@ int main(void)
 	OLED_Init();
 	Key_Init();
 	Timer_Init();
-	Encoder_Init();
 	PWM_Init();
 	Serial_Init();
 	MotorEncoder_Init();
 	
-	
-//		OLED_ShowString(1, 1, "PID");
-//		OLED_ShowString(2, 3, "KP");
-//		OLED_ShowString(3, 3, "KI");
-//		OLED_ShowString(4, 3, "KD");
-//		Cursor_Init2();
 		
 	 while (1)
     {
-			Target = 20;			
+			Target = 200;			
+			OLED_ShowNum(1, 1, Out, 5);
 			
-			Forward();
-			Set_PWMCompare(Out);
-			printf("%f,%f,%f\n",Target,CNT,Out);
-			
-			OLED_ShowString(1, 1, "Encoder Test:");
-    
-    
-    
-        // ??1:????TIM3->CNT,???
-        int16_t encoder_raw = (int16_t)TIM3->CNT;
-        OLED_ShowNum(2, 1, encoder_raw, 5);
-        
-        // ??2:???????
-        int16_t encoder_tick = GetEncoderCount_Tick();
-        OLED_ShowNum(3, 1, encoder_tick, 5);
-        
-        // ??????,????????
-        if(encoder_raw != 0)
-        {
-            OLED_ShowString(4, 1, "Encoder OK!   ");
-        }
-        else
-        {
-            OLED_ShowString(4, 1, "Encoder ERROR!");
-        }
-        
-        Delay_ms(200);
-    
-			
+			printf("%f,%f,%f\n",Target,CNT,Out/1.4);
 		}
 }
 
@@ -78,16 +44,15 @@ void TIM2_IRQHandler(void)     //Õâ¸öµ÷¿ØÖÜÆÚ°¡£¬Ò»°ãÊÇÔ½Ğ¡Ô½ºÃ¡£µ«ÊÇ»áÊÜµ½Ó²¼şÅ
 	if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
 	{
 		Key_Tick();
-		Cursor_Tick2();
 		
-//		kp = LED_Button2_1;
-//		ki = LED_Button2_2;
-//		kd = LED_Button2_3;
+		kp = 0.8;
+		ki = 0;
+		kd = 3;
 		
 		static uint8_t count = 0;
 		count++;
 		
-		if(count >= 10)
+		if(count >= 200)
 	{
 		/**********Î»ÖÃÊ½PIDµ÷¿Ø***********/
 		/*»ñÈ¡Êµ¼ÊÖµ*/
@@ -106,20 +71,21 @@ void TIM2_IRQHandler(void)     //Õâ¸öµ÷¿ØÖÜÆÚ°¡£¬Ò»°ãÊÇÔ½Ğ¡Ô½ºÃ¡£µ«ÊÇ»áÊÜµ½Ó²¼şÅ
 		Out = kp * error0 + ki * ErrorInt + kd * (error0 - error1); 
 		
 		/*Êä³öÏŞ·ù*/
-		if(Out > 100){Out = 100;}
-		if(Out < -100){Out = -100;}
+		if(Out > 200){Out = 200;}
+		if(Out < -200){Out = -200;}
+		
 		
 		/*Ö´ĞĞ¿ØÖÆ*/
-//		if(Out > 0)
-//		{
-//			Forward();
-//			Set_PWMCompare((uint16_t)Out);   //Êä³öÖÁ±»¿Ø¶ÔÏó£¬¶ÔÓÚµç»úÀ´Ëµ£¬±ãÊÇPWM
-//		}
-//		else
-//		{
-//			Backward();
-//			Set_PWMCompare((uint16_t)(-Out));
-//		}
+		if(Out > 0)
+		{
+			Forward();
+			Set_PWMCompare((uint16_t)Out);   //Êä³öÖÁ±»¿Ø¶ÔÏó£¬¶ÔÓÚµç»úÀ´Ëµ£¬±ãÊÇPWM
+		}
+		else
+		{
+			Backward();
+			Set_PWMCompare((uint16_t)(-Out));
+		}
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 	}
 }
